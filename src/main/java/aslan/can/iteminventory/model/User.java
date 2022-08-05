36da6648a0d729f567e4cf507b2f475fe8722f30
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-05 12:42:01 +0300

    First model

---
+++ src/main/java/aslan/can/iteminventory/model/User.java
@@ -0,0 +1,22 @@
+package aslan.can.iteminventory.model;
+
+import java.util.UUID;
+
+public class User {
+    
+    private final UUID id;
+    private final String userName;
+
+    public User(UUID id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
