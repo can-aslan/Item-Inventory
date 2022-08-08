f990afe33d1cb58f117f401098189ed1794f0af8
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-08 16:03:23 +0300

    Added Oracle DB Dao

---
+++ src/main/java/aslan/can/iteminventory/dao/OracleDBUserDao.java
@@ -0,0 +1,55 @@
+package aslan.can.iteminventory.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.stereotype.Repository;
+import aslan.can.iteminventory.model.User;
+
+@Repository("oracle")
+public class OracleDBUserDao implements UserDao {
+
+    private static List<User> DB = new ArrayList<>();
+
+    @Override
+    public int insertUser(UUID id, User user) {
+        DB.add(new User(id, user.getUserName()));
+        return 1;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return DB;
+    }
+
+    @Override
+    public Optional<User> selectUserByID(UUID id) {
+        return DB.stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deleteUserByID(UUID id) {
+        Optional<User> userToDelete = selectUserByID(id);
+
+        // If the specified user is not found, return 0
+        if (userToDelete.isEmpty()) return 0;
+
+        // Delete the specified user
+        DB.remove(userToDelete.get());
+        return 1;
+    }
+
+    @Override
+    public int updateUserByID(UUID id, User user) {
+        return selectUserByID(id).map(userToUpdate -> {
+            int indexOfUserToUpdate = DB.indexOf(userToUpdate);
+            if (indexOfUserToUpdate < 0) return 0; // If userToUpdate is not found, return 0
+
+            // Update user
+            DB.set(indexOfUserToUpdate, new User(id, user.getUserName()));
+
+            return 1;
+        }).orElse(0);
+    }
+}
