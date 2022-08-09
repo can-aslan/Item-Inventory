7a222e4a448a1da96e7caca2a6ef750ec0d7a5c3
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-09 10:44:56 +0300

    Changed OracleDB implementation

--- src/main/java/aslan/can/iteminventory/IteminventoryApplication.java
+++ src/main/java/aslan/can/iteminventory/IteminventoryApplication.java
@@ -2,12 +2,21 @@ package aslan.can.iteminventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import aslan.can.iteminventory.dao.OracleDBUserDao;
 
 @SpringBootApplication
 public class IteminventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IteminventoryApplication.class, args);
+		try {
+			OracleDBUserDao.establishConnection();
+			System.out.println("SUCCESS: Oracle DB connection established.");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR: Could not connect to Oracle DB.");
+		}
 	}
 
 }
