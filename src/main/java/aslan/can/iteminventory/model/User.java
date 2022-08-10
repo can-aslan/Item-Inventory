bc5576054e171ef3bf5ba01718641ce054d3e26c
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-10 15:21:16 +0300

    Adding items to users

--- src/main/java/aslan/can/iteminventory/model/User.java
+++ src/main/java/aslan/can/iteminventory/model/User.java
@@ -1,5 +1,6 @@
 package aslan.can.iteminventory.model;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -8,6 +9,13 @@ public class User {
     
     private final UUID id;
     private final String userName;
+    private ArrayList<String> items;
+
+    public User() {
+        id = null;
+        userName = "null";
+        items = null;
+    }
 
     public User(@JsonProperty("id") UUID id,
                 @JsonProperty("userName") String userName) {
@@ -19,6 +27,23 @@ public class User {
         else {
             this.userName = userName;
         }
+
+        items = new ArrayList<String>();
+    }
+
+    public User(@JsonProperty("id") UUID id,
+                @JsonProperty("userName") String userName,
+                @JsonProperty("items") ArrayList<String> items) {
+        this.id = id;
+
+        if (userName == null || userName.isEmpty()) {
+            this.userName = "No Username Provided";
+        }
+        else {
+            this.userName = userName;
+        }
+
+        this.items = items;
     }
 
     public UUID getId() {
@@ -27,5 +52,20 @@ public class User {
 
     public String getUserName() {
         return userName;
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+    }
+
+    public boolean removeItem(String item) {
+        int removeIndex = -1;
+
+        if (!items.stream().filter(potentialItemToRemove -> potentialItemToRemove.equals(item)).findFirst().isEmpty()) {
+            items.remove(item);
+            return true;
+        }
+
+        return false;
     }
 }
