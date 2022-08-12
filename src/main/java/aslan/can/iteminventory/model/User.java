27c236dfd2cfeba2a9bd589ff48dd9c2141a7d89
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-12 14:22:59 +0300

    Removed user item

--- src/main/java/aslan/can/iteminventory/model/User.java
+++ src/main/java/aslan/can/iteminventory/model/User.java
@@ -1,6 +1,5 @@
 package aslan.can.iteminventory.model;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,15 +8,6 @@ public class User {
     
     private final UUID id;
     private final String userName;
-    // private ArrayList<String> items;
-
-    /*
-    public User() {
-        id = null;
-        userName = "null";
-        items = null;
-    }
-    */
 
     public User(@JsonProperty("id") UUID id,
                 @JsonProperty("userName") String userName) {
@@ -29,26 +19,7 @@ public class User {
         else {
             this.userName = userName;
         }
-
-        // items = new ArrayList<String>();
     }
-
-    /* 
-    public User(@JsonProperty("id") UUID id,
-                @JsonProperty("userName") String userName,
-                @JsonProperty("items") ArrayList<String> items) {
-        this.id = id;
-
-        if (userName == null || userName.isEmpty()) {
-            this.userName = "No Username Provided";
-        }
-        else {
-            this.userName = userName;
-        }
-
-        this.items = items;
-    }
-    */
 
     public UUID getId() {
         return id;
@@ -57,21 +28,4 @@ public class User {
     public String getUserName() {
         return userName;
     }
-
-    /*
-    public void addItem(String item) {
-        items.add(item);
-    }
-
-    public boolean removeItem(String item) {
-        int removeIndex = -1;
-
-        if (!items.stream().filter(potentialItemToRemove -> potentialItemToRemove.equals(item)).findFirst().isEmpty()) {
-            items.remove(item);
-            return true;
-        }
-
-        return false;
-    }
-    */
 }
