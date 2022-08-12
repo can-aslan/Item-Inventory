5e331b1f56cb0373215388382a8a67c72f3b3552
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-12 14:36:40 +0300

    commit

--- src/main/java/aslan/can/iteminventory/IteminventoryApplication.java
+++ src/main/java/aslan/can/iteminventory/IteminventoryApplication.java
@@ -2,9 +2,11 @@ package aslan.can.iteminventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 import aslan.can.iteminventory.dao.OracleDBUserDao;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IteminventoryApplication {
 
 	public static void main(String[] args) {
