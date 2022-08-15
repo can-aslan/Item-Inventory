fb341a185f2537c672101bbf737dbed0fe5c1db0
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-08-15 21:30:30 +0300

    Deprecated ArrayListUserDao

--- src/main/java/aslan/can/iteminventory/dao/ArrayListUserDao.java
+++ src/main/java/aslan/can/iteminventory/dao/ArrayListUserDao.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.stereotype.Repository;
 import aslan.can.iteminventory.model.User;
 
+@Deprecated
 @Repository("arrayList-user")
 public class ArrayListUserDao implements UserDao {
 
